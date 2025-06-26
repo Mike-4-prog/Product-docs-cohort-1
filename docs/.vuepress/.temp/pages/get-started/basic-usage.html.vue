2b9347bc457dee8b1322e0c95e8a9885e0f9c1e0
Richard-Emmanuel <richardnuelofficial@gmail.com>
2025-06-26 14:54:22 +0100

    update git ignore

--- docs/.vuepress/.temp/pages/get-started/basic-usage.html.vue
+++
@@ -1,5 +0,0 @@
-<template><div><h1 id="getting-started-on-this-page" tabindex="-1"><a class="header-anchor" href="#getting-started-on-this-page"><span>Getting started on this page</span></a></h1>
-<p>This is a test content...</p>
-</div></template>
-
-
--- docs/.vuepress/.temp/pages/get-started/introduction.html.vue
+++
@@ -1,5 +0,0 @@
-<template><div><h1 id="introduction" tabindex="-1"><a class="header-anchor" href="#introduction"><span>Introduction</span></a></h1>
-<p>content here...</p>
-</div></template>
-
-
