cd7daaad483b11e76807ff1c9b09e2f253300832
Fsquared123 <feliciafranklin027@gmail.com>
2025-06-27 14:06:09 +0100

    Felicia's first commit

--- docs/.vuepress/.temp/markdown-tab/config.js
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -1,6 +1,6 @@
-import { CodeTabs } from "C:/Users/Admin/Downloads/Docs/Product-docs-cohort-1/.yarn/__virtual__/@vuepress-plugin-markdown-tab-virtual-787e54d224/4/AppData/Local/Yarn/Berry/cache/@vuepress-plugin-markdown-tab-npm-2.0.0-rc.86-b98fa3c044-10c0.zip/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
-import { Tabs } from "C:/Users/Admin/Downloads/Docs/Product-docs-cohort-1/.yarn/__virtual__/@vuepress-plugin-markdown-tab-virtual-787e54d224/4/AppData/Local/Yarn/Berry/cache/@vuepress-plugin-markdown-tab-npm-2.0.0-rc.86-b98fa3c044-10c0.zip/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
-import "C:/Users/Admin/Downloads/Docs/Product-docs-cohort-1/.yarn/__virtual__/@vuepress-plugin-markdown-tab-virtual-787e54d224/4/AppData/Local/Yarn/Berry/cache/@vuepress-plugin-markdown-tab-npm-2.0.0-rc.86-b98fa3c044-10c0.zip/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+import { CodeTabs } from "/Users/user/Openmadness/Product-docs-cohort-1/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "/Users/user/Openmadness/Product-docs-cohort-1/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "/Users/user/Openmadness/Product-docs-cohort-1/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
 
 export default {
   enhance: ({ app }) => {
--- docs/.vuepress/.temp/pages/get-started/basic-usage.html.js
+++ docs/.vuepress/.temp/pages/get-started/basic-usage.html.js
@@ -1,5 +1,5 @@
-import comp from "C:/Users/Admin/Downloads/Docs/Product-docs-cohort-1/docs/.vuepress/.temp/pages/get-started/basic-usage.html.vue"
-const data = JSON.parse("{\"path\":\"/get-started/basic-usage.html\",\"title\":\"Basic Usage\",\"lang\":\"en-US\",\"frontmatter\":{\"lang\":\"en-US\",\"title\":\"Basic Usage\",\"description\":\"Description of this page\"},\"headers\":[],\"git\":{},\"filePathRelative\":\"get-started/basic-usage.md\"}")
+import comp from "/Users/user/Openmadness/Product-docs-cohort-1/docs/.vuepress/.temp/pages/get-started/basic-usage.html.vue"
+const data = JSON.parse("{\"path\":\"/get-started/basic-usage.html\",\"title\":\"Basic Usage\",\"lang\":\"en-US\",\"frontmatter\":{\"lang\":\"en-US\",\"title\":\"Basic Usage\",\"description\":\"Description of this page\"},\"headers\":[],\"git\":{\"updatedTime\":1750507911000,\"contributors\":[{\"name\":\"Richard-Emmanuel\",\"username\":\"Richard-Emmanuel\",\"email\":\"richardnuelofficial@gmail.com\",\"commits\":1,\"url\":\"https://github.com/Richard-Emmanuel\"}],\"changelog\":[{\"hash\":\"8fdd7fc642897274d460b071cd9501cf91c37820\",\"time\":1750507911000,\"email\":\"richardnuelofficial@gmail.com\",\"author\":\"Richard-Emmanuel\",\"message\":\"Initial Project Setup\"}]},\"filePathRelative\":\"get-started/basic-usage.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
