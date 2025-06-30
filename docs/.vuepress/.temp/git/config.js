3ec1482dc33bf655f19eee15722a61dbba0f6f84
Fsquared123 <feliciafranklin027@gmail.com>
2025-06-30 01:49:54 +0100

    Resolved conflicts with the first Custom Pipelines content

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/Users/user/Openmadness/Product-docs-cohort-1/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/Users/user/Openmadness/Product-docs-cohort-1/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
